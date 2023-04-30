d5286ffdc49f7e73468d8d09334f94b4345badfa
NishantSingh02 <nst7372@gmail.com>
2023-04-30 17:05:00 +0530

    groupby added

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,18 +1,19 @@
 import { useState, useEffect, useRef } from "react";
 import { projectFireStore } from "../firebase/config";
+import { type } from "@testing-library/user-event/dist/type";
 
 export const useCollection = (
   collection,
   _query,
   compare,
-  queryt,
+  timeQuery,
   option,
-  order
+  order,
+  groupBy
 ) => {
   const [document, setDocument] = useState(null);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
-
   const query = useRef(_query).current;
 
   useEffect(() => {
@@ -21,11 +22,12 @@ export const useCollection = (
     if (query) {
       ref = ref.where(...query);
     }
-    if (queryt) {
-      ref = ref.where(option, compare, queryt);
+
+    if (timeQuery) {
+      ref = ref.where("createdAt", compare, timeQuery);
     }
 
-    if (order && option) {
+    if (option === "createdAt" && option) {
       ref = ref.orderBy(option, order);
     }
 
@@ -37,6 +39,32 @@ export const useCollection = (
         snapshot.docs.forEach((doc) => {
           results.push({ ...doc.data(), id: doc.id });
         });
+
+        if (option === "amount") {
+          if (order === "desc") {
+            results.sort((a, b) => b.amount - a.amount);
+          } else {
+            results.sort((a, b) => a.amount - b.amount);
+          }
+        }
+
+        if (groupBy) {
+          results = results.reduce((result, transaction) => {
+            const name = transaction[groupBy];
+            if (!result[name]) {
+              result[name] = [];
+            }
+            result[name].push(transaction);
+            return result;
+          }, {});
+
+          results = Object.keys(results)
+            .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+            .reduce((sortedResults, key) => {
+              sortedResults[key] = results[key];
+              return sortedResults;
+            }, {});
+        }
 
         setDocument(results);
         setIsPending(false);
@@ -50,7 +78,7 @@ export const useCollection = (
 
     //unsubscribe on unmount
     return () => unsubscribe();
-  }, [collection, query, order, option, queryt]);
+  }, [collection, query, order, option, timeQuery, groupBy]);
 
   return { document, isPending, error };
 };
