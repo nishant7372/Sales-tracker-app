29e429d6b296d4280ec47cf0f19ca260f4fda76d
NishantSingh02 <nst7372@gmail.com>
2023-04-30 17:03:55 +0530

    filter options added

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -14,51 +14,68 @@ export default function Home() {
   const oneDayAgo = Math.trunc(Number(now) - 86400000);
 
   const { user } = useAuthContext();
+
   const [option, setOption] = useState("createdAt");
   const [order, setOrder] = useState("desc");
-  const [query, setQuery] = useState(oneDayAgo);
+  const [groupBy, setGroupBy] = useState(null);
+
+  const [timeQuery, setTimeQuery] = useState(oneDayAgo);
+
   const [activeId, setActiveId] = useState(null);
   const [showConfirm, setShowConfirm] = useState(false);
 
   const { deleteDocument } = useFirestore("transactions");
 
   const deleteDoc = (response, id) => {
-    console.log(id);
     if (response) {
       deleteDocument(id);
     }
     setShowConfirm(false);
   };
 
-  const handleOptionChange = (e) => {
-    setOption(e.target.value);
-    if (e.target.value === "amount") setQuery(0);
-    else setQuery(oneDayAgo);
+  const handleTimeOptionChange = (val) => {
+    if (val === "all") {
+      setTimeQuery(null);
+    } else if (val === "last24hr") {
+      setTimeQuery(oneDayAgo);
+    }
   };
 
-  const handleOrderChange = (e) => {
-    setOrder(e.target.value);
+  const handleGroupOptionChange = (val) => {
+    if (val === "none") {
+      setGroupBy(null);
+    } else setGroupBy(val);
+  };
+
+  const handleOptionChange = (val) => {
+    setOption(val);
+    setOrder("desc");
+  };
+
+  const handleOrderChange = (val) => {
+    setOrder(val);
   };
 
   const { document, isPending, error } = useCollection(
     "transactions",
     ["uid", "==", user.uid],
     ">=",
-    query,
+    timeQuery,
     option,
-    order
+    order,
+    groupBy
   );
 
   let totalAmount = 0;
   let totalPayment = 0;
-  function calcAmountandPayment() {
+  const calcAmountandPayment = () => {
     if (document) {
       for (let i = 0; i < document.length; i++) {
         totalPayment += Number(document[i].payment);
         totalAmount += Number(document[i].amount);
       }
     }
-  }
+  };
   calcAmountandPayment();
 
   return (
@@ -66,31 +83,59 @@ export default function Home() {
       <div className={styles[`transaction-list`]}>
         <Slide triggerOnce direction="down">
           <div className={styles["title-container"]}>
-            <div className={styles[`title`]}>
-              <div>Transactions:</div>
+            <div className={styles["title"]}>💰Transactions</div>
+            <div>
               <div className={styles["filter"]}>
                 <div>
-                  <select onChange={handleOptionChange}>
-                    <option value="createdAt">Created</option>
-                    <option value="amount">Amount</option>
+                  <select
+                    onChange={(e) => handleTimeOptionChange(e.target.value)}
+                  >
+                    <option value="last24hr">Last 24 hours</option>
+                    <option value="all">All Transactions</option>
                   </select>
                 </div>
-                {option === "amount" && (
+                <div className={styles["filter-right"]}>
+                  <div>GroupBy:</div>
+                  <select
+                    onChange={(e) => handleGroupOptionChange(e.target.value)}
+                  >
+                    <option value="none">None</option>
+                    <option value="name">Name</option>
+                    <option value="work">Work</option>
+                  </select>
+                </div>
+
+                <div className={styles["filter-right"]}>
                   <div>
-                    <select onChange={handleOrderChange}>
-                      <option value="desc">Descending</option>
-                      <option value="asc">Ascending</option>
+                    <select
+                      onChange={(e) => handleOptionChange(e.target.value)}
+                    >
+                      <option value="createdAt">Created</option>
+                      <option value="amount">Amount</option>
                     </select>
                   </div>
-                )}
-                {option === "createdAt" && (
-                  <div>
-                    <select onChange={handleOrderChange}>
-                      <option value="desc">MostRecent</option>
-                      <option value="asc">LeastRecent</option>
-                    </select>
-                  </div>
-                )}
+
+                  {option === "amount" && (
+                    <div>
+                      <select
+                        onChange={(e) => handleOrderChange(e.target.value)}
+                      >
+                        <option value="desc">Descending</option>
+                        <option value="asc">Ascending</option>
+                      </select>
+                    </div>
+                  )}
+                  {option === "createdAt" && (
+                    <div>
+                      <select
+                        onChange={(e) => handleOrderChange(e.target.value)}
+                      >
+                        <option value="desc">MostRecent</option>
+                        <option value="asc">LeastRecent</option>
+                      </select>
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
           </div>
@@ -98,7 +143,24 @@ export default function Home() {
         {isPending && <div>Loading Transactions...</div>}
         {error && <div>{error}</div>}
         <Fade>
-          {document && (
+          {document &&
+            groupBy &&
+            !Array.isArray(document) &&
+            Object.entries(document).map(([key, value]) => (
+              <div key={key} className={styles["groupby-list"]}>
+                <div className={styles["group-key"]}>
+                  <div className={styles["key-name"]}>{key}</div>
+                </div>
+                <ul>
+                  <TransactionList
+                    document={value}
+                    setActiveId={setActiveId}
+                    setShowConfirm={setShowConfirm}
+                  />
+                </ul>
+              </div>
+            ))}
+          {groupBy === null && document && Array.isArray(document) && (
             <TransactionList
               document={document}
               setActiveId={setActiveId}
@@ -108,13 +170,10 @@ export default function Home() {
         </Fade>
       </div>
       <div className={styles["right-section"]}>
-        <div>
-          <Link to="/newcustomer">Add new Transaction</Link>
-        </div>
-        <div>
-          <PieChart totalAmount={totalAmount} totalPayment={totalPayment} />
-        </div>
+        <Link to="/new">Add new Transaction</Link>
+        <PieChart totalAmount={totalAmount} totalPayment={totalPayment} />
       </div>
+
       <Fade duration={500} triggerOnce>
         <>{showConfirm && <Confirm id={activeId} deleteDoc={deleteDoc} />}</>
       </Fade>
