cec4bac93c27dba8917b98f2620921246cc720da
NishantSingh02 <nst7372@gmail.com>
2023-05-07 16:50:41 +0530

    update

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import "firebase/auth";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD2N63uAVXRt2ZkTaimYx8BB0cjZZ9QN74",
-  authDomain: "sales-tracker-a4c71.firebaseapp.com",
-  projectId: "sales-tracker-a4c71",
-  storageBucket: "sales-tracker-a4c71.appspot.com",
-  messagingSenderId: "1058629363909",
-  appId: "1:1058629363909:web:8da80c6c1903f9902a28ac",
-  measurementId: "G-CKL8S8WN46",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 //init firebase
